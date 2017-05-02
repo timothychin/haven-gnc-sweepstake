0407096ecac936deea2bb185c2e872a5b820b665
Tim Chin <timm.chin@gmail.com>
2017-05-01 23:24:22 -0700

    Adds working phoneCheck func that skips to next input

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // Skip to next box when a phone number box is filled to size
 var phoneCheck = function(phoneNum) {
-  console.log('triggered')
-  if (phoneNum.TextLength >= 3) {
-    var next = document.getElementById('phone' + (phoneNum.id + 1));
-    console.log(next);
-    next.focus();
+  if (typeof(Number(phoneNum.value)) !== true) {
+    // Prompts for proper phone number
+  }
+  if (phoneNum.value.length >= 3) {
+    // Increments to next phoneID
+    document.getElementById('phone' + (Number(phoneNum.id.slice(5)) + 1)).focus();
   }
 };
+
+// Checks that proper values are put into every input
+var inputCheck = function(input) {
+  if (input.value != true) {
+
+  }
+};
+
+// Response triggered for bad inputs
+var badInput = function(element) {
+
+}
